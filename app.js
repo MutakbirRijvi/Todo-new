52803dbd34457cf5b45f26e64f387650ad2fad65
Mutakbir Rijvi <36194787+MutakbirRijvi@users.noreply.github.com>
2024-02-20 00:47:36 +0600

    Update app.js
    Localstorage updated.

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const form = document.getElementById('form');
 const input = document.getElementById('input');
-const todos = document.getElementById('todos');
+const todosUL = document.getElementById('todos');
+
+const todos = JSON.parse(localStorage.getItem('todos'));
+
+if(todos){
+
+    todos.forEach(todo=>{
+        addtodo(todo);
+    });
+    
+}
 
 form.addEventListener('submit', (e) =>{
     e.preventDefault();
@@ -9,27 +19,39 @@ form.addEventListener('submit', (e) =>{
     
 });
 
-function addtodo(){
-    const todoText = input.value;
+function addtodo(todo){
+    let todoText = input.value;
+
+    if(todo){
+        todoText = todo.text;
+    }
 
     if(todoText){
         
         const todoEl = document.createElement("li");
+
+        if(todo && todo.completed){
+            todoEl.classList.add("completed");
+        }
+
         todoEl.innerText = todoText;
 
         todoEl.addEventListener('click', () => {
             todoEl.classList.toggle('completed');
+            updateLS();
         });
 
         todoEl.addEventListener('contextmenu' , (e) =>{
             e.preventDefault();
             todoEl.remove();
+            updateLS();
         });
 
-        todos.appendChild(todoEl);
+        todosUL.appendChild(todoEl);
 
         input.value = "";
 
+        updateLS();
     }
 }
 
@@ -38,10 +60,10 @@ function updateLS(){
 
     const todos = [];
     
-    todosEl.forEach((todosEl) =>{
+    todosEl.forEach((todoEl) =>{
         todos.push({
-            text: todosEl.innerText,
-            completed: todosEl.classList.contains("completed"),
+            text: todoEl.innerText,
+            completed: todoEl.classList.contains("completed"),
         });
     });
 
